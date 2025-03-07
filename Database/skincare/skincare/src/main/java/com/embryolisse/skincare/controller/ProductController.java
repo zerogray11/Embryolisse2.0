e86bdeff8447fff03f2208ddb304fac66065a197
Farhat Zafar <farhatzafar63@gmail.com>
2025-03-07 10:03:57 -0500

    add webconfig file for allowing cors with frontend port

--- Database/skincare/skincare/src/main/java/com/embryolisse/skincare/controller/ProductController.java
+++ Database/skincare/skincare/src/main/java/com/embryolisse/skincare/controller/ProductController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173") //This is really important to tell it how to connect to REACT Front-end
+@CrossOrigin(origins = "http://localhost:5174") //This is really important to tell it how to connect to REACT Front-end
 @RequestMapping("api/products")
 public class ProductController {
 
