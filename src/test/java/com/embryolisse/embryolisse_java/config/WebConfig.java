646887e5357c521bad5d2a0d170af6fcb0f01e46
Zero <zerogray11@gmail.com>
2025-03-14 12:53:31 -0400

    web config update

---
+++ src/test/java/com/embryolisse/embryolisse_java/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.embryolisse.embryolisse_java.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://embryolissemiseajour.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
+
+
+
