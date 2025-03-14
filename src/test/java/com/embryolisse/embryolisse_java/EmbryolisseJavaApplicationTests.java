3a0da197731d5b04fcd72bbb9a4829c76a95ae88
Zero <zerogray11@gmail.com>
2025-03-14 14:20:48 -0400

    idk

--- src/test/java/com/embryolisse/embryolisse_java/EmbryolisseJavaApplicationTests.java
+++ src/test/java/com/embryolisse/embryolisse_java/EmbryolisseJavaApplicationTests.java
@@ -3,7 +3,7 @@ package com.embryolisse.embryolisse_java;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = EmbryolisseJavaApplication.class)
 class EmbryolisseJavaApplicationTests {
 
 	@Test
